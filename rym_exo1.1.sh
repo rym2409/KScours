d997b24c27648fc297b02f451f260edd7974d7f0
unknown <rym.siamer@gmail.com>
2021-01-18 11:56:03 +0100

    ajout dans la 1ere fonction

---
+++ rym_exo1.1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir MOI
+cd MOI
+mkdir MesDMs
+mkdir MesLettres
+cd MesLettres
+touch Motiv.doc
+touch cv.doc
+cd /mnt/c/script/MOI
+mkdir MesTps
+cd MesTps
+mkdir TpsUNIX
+mkdir TPsC
+cd TPsC
+touch Tp1.c
+touch Tp2.c
+mkdir Executables
